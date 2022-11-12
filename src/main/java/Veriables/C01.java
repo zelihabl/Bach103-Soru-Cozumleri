ab248a276df4e451191344d3ebad46831d2a576f
Zeliha Balı <zelihabl98@gmail.com>
2022-11-12 10:41:16 +0300

    string

---
+++ src/main/java/Veriables/C01.java
@@ -0,0 +1,49 @@
+package Veriables;
+
+public class C01 {
+    public static void main(String[] args) {
+
+        String name = "Zeliha";
+        System.out.println(name);
+
+        Long a = 124L;
+        Long b = 122L;
+        String s = "sevgi";
+        System.out.println(a+b+"s");
+        System.out.println(s+" " + (a*b));
+
+        boolean c = true;
+        boolean d = false;
+        System.out.println(c + " "+ d);
+
+
+        short x= 12;
+        int yeni = x;
+        System.out.println(yeni);
+
+        long  yas = 15L;
+        int yeniYas = (int) yas;
+
+        short min =Short.MIN_VALUE;
+        byte max = Byte.MAX_VALUE;
+
+        System.out.println(min+ max);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ src/main/java/ifStatement/C01.java
@@ -0,0 +1,59 @@
+package ifStatement;
+
+public class C01 {
+    public static void main(String[] args) {
+
+        /*
+        Aşağıdaki seçenekler doğrultusunda gereken kodu yazınız.
+       a) Aralık, Ocak, Şubat için "Kış"
+       b) Mart, Nisan, Mayıs için "İlkbahar"
+       c) Haziran, Temmuz, Ağustos için "Yaz"
+       d) Eylül, Ekim, Kasım için "Sonbahar"
+       e) Diğerleri için "Geçersiz ay adı"
+
+         */
+
+
+
+
+        //2) Aşağıdaki seçenekler doğrultusunda gereken kodu yazınız.
+        //a) Şifre boşluk karakterinden farklı en az 8 karakter içeriyorsa "Geçerli Şifre"
+        //b) Herhangi bir pozisyonda şifrede boşluk karakteri varsa "Şifrede boşluk karakteri kullanmayınız"
+        //c) a ve b koşullarının sağlanmaması durumunda "Geçersiz Şifre"
+
+         /*
+        3) Ayın numarasını girdiğinizde ayın adını yazdırmak için gereken kodu yazınız.
+        Örnek; 1 için çıktı: "Ocak", 2 için çıktı: "Şubat" vb.
+         */
+
+
+        //4) Aşağıdaki seçenekler doğrultusunda gereken kodu yazınız.
+        //a) Son basamak 5 ‘e eşit ve 5 den büyükse, yuvarlama işlemi: “Son basamağı bir üst ondalığa yuvarla”
+        //b) Son basamak 5 den küçükse, yuvarlama işlemi: “Son basamağı bir alt ondalığa yuvarla”
+
+        /*
+        5) Aşağıdaki seçenekler doğrultusunda gereken kodu yazınız.
+        a) Bir üçgenin iki kenarının uzunluğu eşitse "İkizkenar Üçgen".
+        b) Bir üçgenin tüm kenarlarının uzunluğu eşitse "Eşkenar Üçgen".
+        c) a ve b koşulları sağlanmıyorsa "Çeşit Kenar Üçgen"
+         */
+
+
+
+        //7)Tam isim için dilbilgisi kurallarını kontrol etmek için kodu yazınız.
+        //a) Kodunuz "ali Can", "Ali can", "ali can" için "Baş harflerinde hata" yazmalıdır.
+        //b) Kodunuz, aşağıdaki gibi tek kelimeler için "Ad veya soyadı eksik" yazmalıdır.
+        //c) "Ali" veya "Can" veya "ali"Kodunuz "ALI CAN" gibi tüm formatlar için "Format hatası" yazmalıdır.
+        //d) Kodunuz bir veya daha fazla boşluk (" " veya " " gibi karakterler) için " Isim girilmedi"
+        //yazmalıdır.
+        //e) Ad, harfler ve boşluktan başka herhangi bir karakteri içeriyorsa, kodunuz "Geçersiz Ad"
+        //yazmalıdır.
+        //Not : Kısaltmada 1'den fazla hata varsa, ilgili tüm hata mesajları çıktı olarak konsolda
+        //görünmelidir. Örneğin; "ali3" için kodunuz "Baş harflerinde hata", "Ad veya soyadı eksik" ve
+        //"Geçersiz Ad"
+
+
+
+
+    }
+}
